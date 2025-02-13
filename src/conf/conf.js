4e3df9c9417e373e5b0bd0f5d3d9b6f08ae84b44
sandeepd <geelucknow@gmail.com>
2025-02-12 21:49:49 -0800

    mapMyIndiaApi, mapMyIndiaAccessToken and locationiqApi

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -6,7 +6,10 @@ const conf = {
     refreshTokenExpiry: String(process.env.REFRESH_TOKEN_EXPIRY),
     cloudinaryApiSecret: String(process.env.CLOUDINARY_API_SECRET),
     cloudinaryName: String(process.env.CLOUDINARY_CLOUD_NAME),
-    cloudinaryApiKey: String(process.env.CLOUDINARY_API_KEY)
+    cloudinaryApiKey: String(process.env.CLOUDINARY_API_KEY),
+    mapMyIndiaApi:String(process.env.MAP_MY_INDIA_API),
+    mapMyIndiaAccessToken:String(process.env.MAP_MY_INDIA_ACCESS_TOKEN),
+    locationiqApi:String(process.env.LOCATIONIQ_API)
 }
 
 export default conf;
