bd7a8de39e76df9dee07c2e8472aa022c178caed
sandeepd <geelucknow@gmail.com>
2025-02-12 21:38:06 -0800

    get forward geocoding route  add

--- src/routes/map.routes.js
+++ src/routes/map.routes.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import { query } from 'express-validator';
 import verifyJWT from '../middlewares/authMiddleware.js';
-import { getSuggestion } from '../controllers/map.controller.js';
+import { getGeocoding, getSuggestion } from '../controllers/map.controller.js';
 const router = express.Router()
 
 router.route("/get-suggestions").get(
     query("query").isString().isLength({ min: 3 }).withMessage("query contains min 3 charecters"),
     verifyJWT,
     getSuggestion
+)
+
+router.route("/get-forward-geocoding").post(
+    verifyJWT,
+    getGeocoding
 )
 
 export default router
