8a76bd01cf45fde0116a2e66754fa596b2df6ea3
sandeepd <geelucknow@gmail.com>
2025-02-12 21:37:07 -0800

    get geocode controller add

--- src/controllers/map.controller.js
+++ src/controllers/map.controller.js
@@ -1,4 +1,4 @@
-import { getAutoSuggestions } from "../services/maps.service.js";
+import { getAddressCoordinate, getAutoSuggestions } from "../services/maps.service.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
@@ -8,9 +8,19 @@ const getSuggestion = asyncHandler(async (req, res, next) => {
     return res.
         status(200)
         .json(
-            new ApiResponse(200,"suggestion get successfully",suggestions,true)
+            new ApiResponse(200, "suggestion get successfully", suggestions, true)
         )
 })
-export{
-    getSuggestion
+const getGeocoding = asyncHandler(async (req, res, next) => {
+    const { address } = req.body
+    const geocode = await getAddressCoordinate(address)
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Address converted in geocode", geocode, true)
+        )
+})
+export {
+    getSuggestion,
+    getGeocoding
 }
