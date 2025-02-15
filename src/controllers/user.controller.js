19acfc816067a1fecfd437ffa092736250798833
sandeepd <geelucknow@gmail.com>
2025-02-15 05:35:47 -0800

    remove file upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,6 @@ import { User } from "../models/user.models.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { validationResult } from "express-validator"
-import uploadOncloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
 import conf from "../conf/conf.js";
@@ -13,8 +12,6 @@ const generateAccessAndRefreshToken = async (id) => {
         const accessToken = user.generateAccesssToken()
         const refreshToken = user.generateRefreshToken()
         user.refreshToken = refreshToken
-        console.log(accessToken,refreshToken);
-        
         await user.save({ validateBeforeSave: false })
         return { accessToken, refreshToken }
     } catch (error) {
@@ -22,12 +19,6 @@ const generateAccessAndRefreshToken = async (id) => {
     }
 }
 const createUser = asyncHandler(async (req, res) => {
-    const errors = validationResult(req)
-    // if (!errors.isEmpty()) {
-
-    //     throw new ApiError(404, "404 errors ", errors.array())
-
-    // }
     const { firstname, lastname, email, password ,role} = req.body
     const existedUser = await User.findOne({
         $or: [
@@ -38,16 +29,10 @@ const createUser = asyncHandler(async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User already exist")
     }
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
-    const avatar = await uploadOncloudinary(avatarLocalPath)
-    const coverImage = await uploadOncloudinary(coverImageLocalPath)
     const user = await User.create({
         firstname: firstname,
         lastname: lastname,
         role:role,
-        avatar: avatar?.url || "",
-        coverImage: coverImage?.url || "",
         email: email,
         password: password
     })
