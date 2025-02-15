5c0334115699d0c5982b49766bb689779633e8e0
sandeepd <geelucknow@gmail.com>
2025-02-15 05:28:24 -0800

    add getDistanceMatrix controller

--- src/controllers/map.controller.js
+++ src/controllers/map.controller.js
@@ -1,10 +1,14 @@
-import { getAddressCoordinate, getAutoSuggestions } from "../services/maps.service.js";
+import { getAddressCoordinate, getAutoSuggestions, getDistanceTime } from "../services/maps.service.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const getSuggestion = asyncHandler(async (req, res, next) => {
     const { query } = req.query;
+    console.log(query);
+
     const suggestions = await getAutoSuggestions(query)
+    console.log(suggestions);
+
     return res.
         status(200)
         .json(
@@ -20,7 +24,17 @@ const getGeocoding = asyncHandler(async (req, res, next) => {
             new ApiResponse(200, "Address converted in geocode", geocode, true)
         )
 })
+const getDistanceMatrix = asyncHandler(async (req, res, next) => {
+    const { originLng, originLtd, destinationLng, destinationLtd } = req.body
+    const distanceMatrix = await getDistanceTime(originLng, originLtd, destinationLng, destinationLtd)
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, "Distance and duration fetched successfully", distanceMatrix, true)
+        )
+})
 export {
     getSuggestion,
-    getGeocoding
+    getGeocoding,
+    getDistanceMatrix
 }
