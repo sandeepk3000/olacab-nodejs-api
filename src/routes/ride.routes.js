e1a1e3f32466e292434aa0c2573e23cef05e73af
sandeepd <geelucknow@gmail.com>
2025-02-15 04:40:59 -0800

    rides estimate route  add

--- src/routes/ride.routes.js
+++ src/routes/ride.routes.js
@@ -1,8 +1,8 @@
 import express from "express"
 import verifyJWT from "../middlewares/authMiddleware.js"
-import { createRide } from "../controllers/ride.controller.js"
+import { createRide, getRidesEstimate } from "../controllers/ride.controller.js"
 const router = express.Router()
 
 router.route("/create-ride").post(verifyJWT,createRide)
-
+router.route("/estimate").post(verifyJWT,getRidesEstimate)
 export default router
