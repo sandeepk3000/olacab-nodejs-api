329c34d80af595e081fa990d5f86dece5d13a486
sandeepd <geelucknow@gmail.com>
2025-02-15 05:45:16 -0800

    remove file upload middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,16 @@
 import express from "express"
 import { body, check } from "express-validator"
-import { createUser, login } from "../controllers/user.controller.js"
+import { createUser, login, logout, refreshAccessToken } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
+import verifyJWT from "../middlewares/authMiddleware.js"
 const router = express.Router()
 
 router.route("/register-user").post([
     body("email").isEmail().withMessage("Invalid Email"),
     body("password").isLength({ min: 4 }).withMessage("Password must be at least 4 characters long"),
     body("firstname").isLength({ min: 3 }).withMessage("Firstname must be at least 3 characters long"),
-    body("lastname").isLength({ min: 3 }).withMessage("Lastname must be at least 3 characters long")
+    body("lastname").isLength({ min: 3 }).withMessage("Lastname must be at least 3 characters long"),
+    body("role").isEmpty().withMessage("role is required")
 ], upload.fields([
     {
         name: "avatar",
@@ -18,25 +20,12 @@ router.route("/register-user").post([
         name: "coverImage",
         maxCount: 1
     }
-]), [
-    check("avatar")
-        .custom((value, { req }) => {
-            if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
-                throw new Error("Avatar is required.")
-            }
-            return true
-        }),
-    check("coverImage").custom((value, { req }) => {
-        if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-            throw new Error("Cover image is required.")
-        }
-        return true
-    })
-], createUser)
+]), createUser)
 
-router.route("/login").post([
+router.route("/user-login").post([
     body("email").isEmail().withMessage("Invalid Email"),
     body("password").isLength({ min: 4 }).withMessage("Password must be at least 4 characters long"),
 ], login)
-
+router.route("/user-logout").get(verifyJWT, logout)
+router.route("/refreshAccessToken").post(verifyJWT, refreshAccessToken)
 export default router
